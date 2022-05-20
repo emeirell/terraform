2a2b2e9d3190e959c4f15400023f6e7045570014
Eduardo Meirelles <emeirelles@vmware.com>
2022-05-20 11:58:29 -0300

    Update variables.tf
    Added Variable

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -1,8 +1,11 @@
-variable vra_url {
-  type    = string
-  default = "https://api.mgmt.cloud.vmware.com"
+variable "region" {
+  type        = string
+  description = "Select the region."
+  default     = "us-east-1"
 }
 
-variable vra_refresh_token {
-  type = string
+variable "cidr" {
+  type        = string
+  description = "Select the CIDR for the VPC."
+  default     = "10.0.0.0/16"
 }
