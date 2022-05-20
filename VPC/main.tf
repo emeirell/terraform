58336e0a4366574500351995a2b5e707648e2875
Eduardo Meirelles <emeirelles@vmware.com>
2022-05-20 14:48:10 -0300

    Update main.tf
    Completed main descriptor.

--- VPC/main.tf
+++ VPC/main.tf
@@ -14,10 +14,17 @@ provider "aws" {
   region = var.region
 }
 
+# Use data sources allow configuration to be
+# generic for any region
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
+
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" var.vpcName {
   cidr_block = var.cidr
   instance_tenancy = "default"
+  enable_dns_support = var.dnsSupport    #gives you an internal domain name
+  enable_dns_hostnames = var.hostSupport #gives you an internal host name
   
   tags = {
     Name = var.vpcName
