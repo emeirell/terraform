7c1d691d29258edba4219e62146165c1d9ebc444
Eduardo Meirelles <emeirelles@vmware.com>
2022-07-12 16:40:51 -0300

    Create variables.tf
    New file

---
+++ onboard/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type        = string
+  description = "Select the region."
+  default     = "us-east-1"
+}
+
+variable "cidr" {
+  type        = string
+  description = "Select the CIDR for the VPC."
+  default     = "172.16.0.0/16"
+}
+
+variable "vpcName" {
+  type        = string
+  description = "Type the name of your VPC"
+  default     = "MyVPC"
+}
+
+variable "dnsSupport" {
+  type        = string
+  description = "DNS support in the VPC."
+  default     = "true"
+}
+
+variable "hostSupport" {
+  type        = string
+  description = "DNS hostnames in the VPC"
+  default     = "false"
+}
