6fbf8c9ba7e769f9b08a2ffbec5315db8135adb2
Eduardo Meirelles <emeirelles@vmware.com>
2022-07-13 10:16:07 -0300

    Update main.tf
    Fixing vpc_id variable

--- onboard/main.tf
+++ onboard/main.tf
@@ -45,7 +45,7 @@ resource "aws_subnet" "sub-main" {
 }
  # Create a Internet Gateway
  resource "aws_internet_gateway" "prod-igw" {
-    vpc_id = “${aws_vpc.vpc-main.id}”
+    vpc_id = {aws_vpc.vpc-main.id}
     tags {
         Name = var.igName
   }
