985496129b2cd2a3192ee805c717b2ed257774fe
Eduardo Meirelles <emeirelles@vmware.com>
2022-09-29 14:36:59 -0300

    Create main.tf
    New file

---
+++ vpc-array/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.29.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+
+resource "aws_vpc" "vpc_vra" {
+  cidr_block = var.cidr
+
+  tags = merge(
+    "${{ for k, v in var.tags : v.key => v.value }}"
+  
+  )
+}
