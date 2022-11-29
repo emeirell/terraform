b21ff43de9d06393b587f2b64946690b636dd353
Eduardo Meirelles <emeirelles@vmware.com>
2022-11-29 16:20:18 -0300

    Update main.tf
    Completed Project

--- Azure-DevOps/main.tf
+++ Azure-DevOps/main.tf
@@ -1,18 +1,26 @@
 terraform {
   required_providers {
-    azuredevops= {
-      source = "microsoft/azuredevops"
-      version = ">=0.1.0"
+    azuredevops = {
+      source    = "microsoft/azuredevops"
+      version   = ">=0.1.0"
     }
   }
 }
 
 provider "azuredevops"{
-  org_service_url = "https://dev.azure.com/emeirelles/"
+  org_service_url       = "https://dev.azure.com/emeirelles/"
   personal_access_token = var.token
 }
   
 resource "azuredevops_project" "project" {
-  name       = var.projname
-  description        = var.projdesc
+  name        = var.projname
+  description = var.projdesc
+  visibility  = var.visibility
+  features = {
+    "boards"       = var.boards
+    "repositories" = var.repositories
+    "pipelines"    = var.pipelines
+    "testplans"    = var.testplans
+    "artifacts"    = var.artifacts
+  }
 }
