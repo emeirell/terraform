6d761e5b82ad9cc13773b37b465def1943a8cea9
Eduardo Meirelles <emeirelles@vmware.com>
2022-11-29 10:36:54 -0300

    Create variables.tf
    Initial variables file

---
+++ Azure-DevOps/variables.tf
@@ -0,0 +1,11 @@
+variable "projname" {
+  type        = string
+  description = "Nome do Projeto"
+  default     = "Project-01"
+}
+
+variable "projdesc" {
+  type        = string
+  description = "Descrição do Projeto"
+  default     = "Projeto Top Secret"
+}
