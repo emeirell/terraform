04107ab6da46b8df3d8f631c6acf78b73d8ac4e7
Eduardo Meirelles <emeirelles@vmware.com>
2022-11-29 15:29:28 -0300

    Update main.tf
    using azurerm

--- Azure-DevOps/main.tf
+++ Azure-DevOps/main.tf
@@ -1,8 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "microsoft/azuredevops"
-      version = ">=0.1.0"
+      source = "hashicorp/azurerm"
+      #source = "microsoft/azuredevops"
+      #version = ">=0.1.0"
+      version = "3.33.0"
       org_service_url = "https://dev.azure.com/emeirelles/"
       personal_access_token = var.token
     }
