bc61d41137ecda06a3080756eebf14bca7ff65da
Eduardo Meirelles <emeirelles@vmware.com>
2022-11-29 10:35:23 -0300

    Create main.tf
    Initial main.tf

---
+++ Azure-DevOps/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+  }
+}
+
+resource "azuredevops_project" "project" {
+  name       = var.projname
+  description        = var.projdesc
+}
